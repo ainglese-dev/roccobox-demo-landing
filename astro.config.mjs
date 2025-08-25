5b96ba073931fc9a02c2ef6985f4aa3c4ce4578b
ainglese-dev <angel.inglese@gmail.com>
2025-08-24 19:37:30 -0500

    mvp demo ready

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ainglese-dev.github.io',
-  base: '/roccobox-demo-landing',
+  // Uncomment base for production deployment:
+  // base: '/roccobox-demo-landing',
 });
